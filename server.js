c3d0f769949be2a70de1cbeb2aaa1272ab55b52e
Aristotelis Tsiolakis <ar1stotel1stsiol@gmail.com>
2025-05-07 20:08:25 +0300

    created the database and the server for the site and it now works dynamically

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
@@ -8,23 +12,33 @@ const port = 3000;
 const db = new pg.Client({
     user:"postgres",
     host:"localhost",
-    databse:"allLessons",
+    database:"allLessons",
     password:"b2463028496",
     port:"5432"
 });
+
 db.connect();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-
-
-app.get("/",(req,res)=>{
-    res.render("lessons.ejs");
+app.get("/",async(req,res)=>{
+    const result = await db.query("SELECT * FROM lessons ORDER by id ASC");
+    const lessons = result.rows;
+    res.render("lessons.ejs",{lessons:lessons,module_id:lessons[0].module_id});
 });
 
-app.post("/lesson:id",(req,res)=>{
-
+app.post("/select-lesson", async (req,res)=>{
+    let id = parseInt(req.body.lessonId)-1;
+    console.log(id);
+    const result = await db.query("SELECT lesson_file from lessons ORDER by id ASC");
+    const fileName = result.rows;
+    console.log(fileName);
+    res.sendFile(__dirname + `/public/slides/${fileName[id].lesson_file}`,err=>{
+        if(err){
+            console.log("Couldn't sent the file");
+        }
+    });
 });
 
 app.listen(port, () =>{
